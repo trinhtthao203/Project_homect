935065f522f7d038e3bf804cd12d60ae53906503
trinhthao203 <trinhtthao203@gmail.com>
2021-12-14 15:50:49 +0700

    icon leaf-green

--- src/components/routeMap.js
+++ src/components/routeMap.js
@@ -3,9 +3,9 @@ import L from "leaflet";
 import "leaflet-routing-machine";
 //import { createControlComponent } from "@react-leaflet/core";
 import { useMap } from "react-leaflet";
-import UserIcon from "../Leaflet/css/images/blueicon.png";
-import Shadow from "../Leaflet/css/images/marker-shadow.png";
-import CTIcon from "../Leaflet/css/images/redIcon.png";
+import UserIcon from "../Leaflet/css/images/leaf-green.png";
+import Shadow from "../Leaflet/css/images/leaf-shadow.png";
+import CTIcon from "../Leaflet/css/images/leaf-red.png";
 function RouteMap(props) {
   const map = useMap();
   const position2 = L.latLng(props.location);
@@ -30,29 +30,30 @@ function RouteMap(props) {
       showAlternatives: false,
       createMarker: function (i, wp) {
         if (i == 0)
-          return L.marker(
-            wp.latLng,
-            { draggable: true, icon: L.icon({
+          return L.marker(wp.latLng, {
+            draggable: true,
+            icon: L.icon({
               iconUrl: UserIcon,
-                iconSize: [38, 95],
-                iconAnchor: [22, 94],
-                popupAnchor: [-3, -76],
-                shadowUrl: Shadow,
-                shadowSize: [68, 95],
-                shadowAnchor: [22, 94]
-            }) }
-          ).bindPopup("Vị trí của bạn");
-        else return L.marker(wp.latLng,{
-          icon: L.icon({
-            iconUrl: CTIcon,
               iconSize: [38, 95],
               iconAnchor: [22, 94],
               popupAnchor: [-3, -76],
               shadowUrl: Shadow,
               shadowSize: [68, 95],
-              shadowAnchor: [22, 94]
-          }) 
-        }).bindPopup("Vị trí chung cư");
+              shadowAnchor: [22, 94],
+            }),
+          }).bindPopup("Vị trí của bạn");
+        else
+          return L.marker(wp.latLng, {
+            icon: L.icon({
+              iconUrl: CTIcon,
+              iconSize: [38, 95],
+              iconAnchor: [22, 94],
+              popupAnchor: [-3, -76],
+              shadowUrl: Shadow,
+              shadowSize: [68, 95],
+              shadowAnchor: [22, 94],
+            }),
+          }).bindPopup("Vị trí chung cư");
       },
     }).addTo(map);
   }, [map]);
